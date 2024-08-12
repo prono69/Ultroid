5234aaabce57b1519ebd91b7d385e39d031bea4e
prono69 <faltu9851@gmail.com>
2024-08-13 02:56:18 +0530

    added motor

--- plugins/utilities.py
+++ plugins/utilities.py
@@ -141,7 +141,7 @@ collection = db["user_data"]
  
 # Define the UTC timezone and Perth timezone
 utc_tz = pytz.utc
-perth_tz = pytz.timezone(udB.get_key("TIMEZONE")) or pytz.timezone("Asia/Kolkata")
+perth_tz = pytz.timezone("Asia/Kolkata")
  
  
 async def mention_user(user_id):
